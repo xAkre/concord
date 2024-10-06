98ca0ce7dfe528a94e90a1b0aee67db5ace5e2a0
Dawid Szymaniak <dawid.szymaniak.dev@gmail.com>
2024-10-06 13:44:05 +0200

    Declare enums for gateway opcodes

---
+++ concord/types/gateway/common.py
@@ -0,0 +1,56 @@
+import enum
+
+__all__ = (
+    "GatewayOpcode",
+    "GatewaySendOpcode",
+    "GatewayReceiveOpcode",
+)
+
+
+class GatewayOpcode(enum.IntEnum):
+    """
+    See [here](https://discord.com/developers/docs/topics/opcodes-and-status-codes#gateway)
+    for Discord's documentation.
+    """
+
+    DISPATCH = 0
+    HEARTBEAT = 1
+    IDENTIFY = 2
+    PRESENCE_UPDATE = 3
+    VOICE_STATE_UPDATE = 4
+    RESUME = 6
+    RECONNECT = 7
+    REQUEST_GUILD_MEMBERS = 8
+    INVALID_SESSION = 9
+    HELLO = 10
+    HEARTBEAT_ACK = 11
+    REQUEST_SOUNDBOARD_SOUNDS = 31
+
+
+class GatewaySendOpcode(enum.IntEnum):
+    """
+    See [here](https://discord.com/developers/docs/topics/opcodes-and-status-codes#gateway)
+    for Discord's documentation.
+    """
+
+    HEARTBEAT = 1
+    IDENTIFY = 2
+    PRESENCE_UPDATE = 3
+    VOICE_STATE_UPDATE = 4
+    RESUME = 6
+    REQUEST_GUILD_MEMBERS = 8
+    REQUEST_SOUNDBOARD_SOUNDS = 31
+
+
+class GatewayReceiveOpcode(enum.IntEnum):
+    """
+    See [here](https://discord.com/developers/docs/topics/opcodes-and-status-codes#gateway)
+    for Discord's documentation.
+    """
+
+    DISPATCH = 0
+    HEARTBEAT = 1
+    RECONNECT = 7
+    INVALID_SESSION = 9
+    HELLO = 10
+    HEARTBEAT_ACK = 11
