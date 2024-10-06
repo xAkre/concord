ddb68207eea8b9c7482cc52c7af87eb3b447e055
Dawid Szymaniak <dawid.szymaniak.dev@gmail.com>
2024-10-06 15:29:25 +0200

    Create VoiceState type

--- concord/types/resources/voice.py
+++ concord/types/resources/voice.py
@@ -1,6 +1,25 @@
 import typing
 
-__all__ = ("VoiceRegion",)
+from ..common import Snowflake
+from .guild import GuildMember
+
+__all__ = ("VoiceState", "VoiceRegion")
+
+
+class VoiceState(typing.TypedDict):
+    guild_id: typing.NotRequired[Snowflake]
+    channel_id: Snowflake | None
+    user_id: Snowflake
+    member: typing.NotRequired[GuildMember]
+    session_id: str
+    deaf: bool
+    mute: bool
+    self_deaf: bool
+    self_mute: bool
+    self_stream: typing.NotRequired[bool | None]
+    self_video: bool
+    suppress: bool
+    request_to_speak_timestamp: str | None
 
 
 class VoiceRegion(typing.TypedDict):
