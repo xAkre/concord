04051d388b9f345bf5f8f0abe62f2d08677af41a
Dawid Szymaniak <dawid.szymaniak.dev@gmail.com>
2024-10-06 15:09:03 +0200

    Add types for stage instances

---
+++ concord/types/resources/stage_instance.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import enum
+import typing
+
+from ..common import Snowflake
+from .user import User
+
+__all__ = ("StageInstance", "StageInstancePrivacyLevel")
+
+
+class StageInstance(typing.TypedDict):
+    """
+    See [here](https://discord.com/developers/docs/resources/stage-instance#stage-instance-object) for
+    Discord's documentation.
+    """
+
+    id: Snowflake
+    guild_id: Snowflake
+    channel_id: Snowflake
+    topic: str
+    privacy_level: StageInstancePrivacyLevel
+    discoverable_disabled: bool
+    guild_scheduled_event_id: Snowflake | None
+
+
+class StageInstancePrivacyLevel(enum.IntEnum):
+    """
+    See [here](https://discord.com/developers/docs/resources/stage-instance#stage-instance-object-privacy-level) for
+    Discord's documentation.
+    """
+
+    GUILD_ONLY = 2
