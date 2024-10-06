e5af36eec6b91db793c6b691cac4a56dd5cc5d60
Dawid Szymaniak <dawid.szymaniak.dev@gmail.com>
2024-10-06 15:54:59 +0200

    Create PartialEmoji type

--- concord/types/resources/emoji.py
+++ concord/types/resources/emoji.py
@@ -1,13 +1,37 @@
+# mypy: disable-error-code="misc"
+# This is needed because Mypy complains when overriding fields in a TypedDict.
+
 import collections.abc
 import typing
 
 from ..common import Snowflake
 from .user import PartialUser
 
-__all__ = ("Emoji",)
+__all__ = ("PartialEmoji", "Emoji")
 
 
-class Emoji(typing.TypedDict):
+class PartialEmoji(typing.TypedDict):
+    """
+    Represents a partial emoji in Discord.
+
+    A partial emoji is guaranteed to contain only an ID. While not explicitly
+    documented in the official docs, this assumption is recommended by users in
+    the Discord Developers server.
+
+    Other fields may be present but should not be relied upon in a partial emoji object.
+    """
+
+    id: Snowflake | None
+    name: typing.NotRequired[str | None]
+    roles: typing.NotRequired[collections.abc.Sequence[Snowflake]]
+    user: typing.NotRequired[PartialUser]
+    require_colons: typing.NotRequired[bool]
+    managed: typing.NotRequired[bool]
+    animated: typing.NotRequired[bool]
+    available: typing.NotRequired[bool]
+
+
+class Emoji(PartialEmoji):
     """
     See [here](https://discord.com/developers/docs/resources/emoji)
     for Discord's documentation.
@@ -15,9 +39,3 @@ class Emoji(typing.TypedDict):
 
     id: Snowflake | None
     name: str | None
-    roles: typing.NotRequired[collections.abc.Sequence[Snowflake]]
-    user: typing.NotRequired[PartialUser]
-    require_colons: typing.NotRequired[bool]
-    managed: typing.NotRequired[bool]
-    animated: typing.NotRequired[bool]
-    available: typing.NotRequired[bool]
