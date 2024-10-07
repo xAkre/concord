4c5ab188b5a24f766fb8fd1e7efa86170a03148b
Dawid Szymaniak <dawid.szymaniak.dev@gmail.com>
2024-10-07 18:35:35 +0200

    Declare GatewayIdentifyMessage type

--- concord/gateway/types/send.py
+++ concord/gateway/types/send.py
@@ -1,6 +1,19 @@
-import enum
+from __future__ import annotations
 
-__all__ = ("GatewaySendOpcode",)
+import dataclasses
+import enum
+import json
+import typing
+
+from .common import GatewayPresenceUpdate
+
+__all__ = (
+    "GatewaySendOpcode",
+    "GatewayMessage",
+    "GatewayIdentifyMessageData",
+    "GatewayIdentifyMessageConnectionProperties",
+    "GatewayIdentifyMessage",
+)
 
 
 class GatewaySendOpcode(enum.IntEnum):
@@ -16,3 +29,61 @@ class GatewaySendOpcode(enum.IntEnum):
     RESUME = 6
     REQUEST_GUILD_MEMBERS = 8
     REQUEST_SOUNDBOARD_SOUNDS = 31
+
+
+@dataclasses.dataclass(kw_only=True)
+class GatewayMessage[T]:
+    """
+    Represents a message to be sent to the gateway.
+
+    See [here](https://discord.com/developers/docs/topics/gateway-events#send-events)
+    for Discord's documentation.
+    """
+
+    opcode: GatewaySendOpcode
+    data: T
+
+    def serialize(self) -> str:
+        """Serialize the message to a string that can be sent to the gateway."""
+        return json.dumps(
+            {
+                "op": self.opcode,
+                "d": self.data,
+            }
+        )
+
+
+class GatewayIdentifyMessageData(typing.TypedDict):
+    """
+    See [here](https://discord.com/developers/docs/topics/gateway-events#identify)
+    for Discord's documentation.
+    """
+
+    token: str
+    properties: GatewayIdentifyMessageConnectionProperties
+    compress: typing.NotRequired[bool]
+    large_threshold: typing.NotRequired[int]
+    shard: typing.NotRequired[typing.Tuple[int, int]]
+    presence: typing.NotRequired[GatewayPresenceUpdate]
+    intents: typing.NotRequired[int]
+
+
+class GatewayIdentifyMessageConnectionProperties(typing.TypedDict):
+    """
+    See [here](https://discord.com/developers/docs/topics/gateway-events#identify-identify-connection-properties)
+    for Discord's documentation.
+    """
+
+    os: str
+    browser: str
+    device: str
+
+
+@dataclasses.dataclass(kw_only=True)
+class GatewayIdentifyMessage(GatewayMessage[GatewayIdentifyMessageData]):
+    """
+    See [here](https://discord.com/developers/docs/topics/gateway-events#identify-identify-structure)
+    for Discord's documentation.
+    """
+
+    opcode: typing.Literal[GatewaySendOpcode.IDENTIFY] = GatewaySendOpcode.IDENTIFY
