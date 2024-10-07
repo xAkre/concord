36a9a7980cf19e862e37035d007a2f0533c7f9a9
Dawid Szymaniak <dawid.szymaniak.dev@gmail.com>
2024-10-07 17:25:11 +0200

    Create PresenceUpdate gateway type

--- concord/gateway/types/common.py
+++ concord/gateway/types/common.py
@@ -1,4 +1,11 @@
+from __future__ import annotations
+
+import collections.abc
 import enum
+import typing
+
+from concord.types.common import Snowflake
+from concord.types.resources.emoji import Emoji
 
 __all__ = (
     "GatewayOpcode",
@@ -53,3 +60,135 @@ class GatewayIntents(enum.IntFlag):
     AUTO_MODERATION_EXECUTION = 1 << 21
     GUILD_MESSAGE_POLLS = 1 << 24
     DIRECT_MESSAGE_POLLS = 1 << 25
+
+
+class GatewayPresenceUpdate(typing.TypedDict):
+    """
+    See [here](https://discord.com/developers/docs/topics/gateway-events#update-presence-gateway-presence-update-structure)
+    for Discord's documentation.
+    """
+
+    since: int
+    activities: typing.List[GatewayActivity]
+    status: GatewayPresenceStatus
+    afk: bool
+
+
+class GatewayActivity(typing.TypedDict):
+    """
+    See [here](https://discord.com/developers/docs/topics/gateway-events#activity-object-activity-structure)
+    for Discord's documentation.
+    """
+
+    name: str
+    type: GatewayActivityType
+    url: typing.NotRequired[str | None]
+    created_at: int
+    timestamps: typing.NotRequired[GatewayActivityTimestamps]
+    application_id: typing.NotRequired[Snowflake]
+    details: typing.NotRequired[str]
+    state: typing.NotRequired[str]
+    emoji: typing.NotRequired[Emoji]
+    party: typing.NotRequired[GatewayActivityParty]
+    assets: typing.NotRequired[GatewayActivityAssets]
+    secrets: typing.NotRequired[GatewayActivitySecrets]
+    instance: typing.NotRequired[bool]
+    flags: typing.NotRequired[int]
+    buttons: typing.NotRequired[collections.abc.Sequence[GatewayActivityButton]]
+
+
+class GatewayActivityType(enum.IntEnum):
+    """
+    See [here](https://discord.com/developers/docs/topics/gateway-events#activity-object-activity-types)
+    for Discord's documentation.
+    """
+
+    PLAYING = 0
+    STREAMING = 1
+    LISTENING = 2
+    WATCHING = 3
+    CUSTOM = 4
+    COMPETING = 5
+
+
+class GatewayActivityTimestamps(typing.TypedDict):
+    """
+    See [here](https://discord.com/developers/docs/topics/gateway-events#activity-object-activity-timestamps)
+    for Discord's documentation.
+    """
+
+    start: typing.NotRequired[int]
+    end: typing.NotRequired[int]
+
+
+class GatewayActivityParty(typing.TypedDict):
+    """
+    See [here](https://discord.com/developers/docs/topics/gateway-events#activity-object-activity-party)
+    for Discord's documentation.
+    """
+
+    id: typing.NotRequired[str]
+    size: typing.NotRequired[typing.Tuple[int, int]]
+
+
+class GatewayActivityAssets(typing.TypedDict):
+    """
+    See [here](https://discord.com/developers/docs/topics/gateway-events#activity-object-activity-assets)
+    for Discord's documentation.
+    """
+
+    large_image: typing.NotRequired[str]
+    large_text: typing.NotRequired[str]
+    small_image: typing.NotRequired[str]
+    small_text: typing.NotRequired[str]
+
+
+class GatewayActivitySecrets(typing.TypedDict):
+    """
+    See [here](https://discord.com/developers/docs/topics/gateway-events#activity-object-activity-secrets)
+    for Discord's documentation.
+    """
+
+    join: typing.NotRequired[str]
+    spectate: typing.NotRequired[str]
+    match: typing.NotRequired[str]
+
+
+class GatewayActivityFlags(enum.IntFlag):
+    """
+    See [here](https://discord.com/developers/docs/topics/gateway-events#activity-object-activity-flags)
+    for Discord's documentation.
+    """
+
+    INSTANCE = 1 << 0
+    JOIN = 1 << 1
+    SPECTATE = 1 << 2
+    JOIN_REQUEST = 1 << 3
+    SYNC = 1 << 4
+    PLAY = 1 << 5
+    PARTY_PRIVACY_FRIENDS = 1 << 6
+    PARTY_PRIVACY_VOICE_CHANNEL = 1 << 7
+    EMBEDDED = 1 << 8
+
+
+class GatewayActivityButton(typing.TypedDict):
+    """
+    See [here](https://discord.com/developers/docs/topics/gateway-events#activity-object-activity-buttons)
+    for Discord's documentation.
+    """
+
+    label: str
+    url: str
+
+
+class GatewayPresenceStatus(enum.StrEnum):
+    """
+    See [here](https://discord.com/developers/docs/topics/gateway-events#update-presence-status-types)
+    for Discord's documentation.
+    """
+
+    ONLINE = "online"
+    IDLE = "idle"
+    DO_NOT_DISTURB = "dnd"
+    INVISIBLE = "invisible"
+    OFFLINE = "offline"
